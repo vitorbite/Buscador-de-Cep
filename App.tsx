cc1d5235b8dba0fdb2f6ae1611a68ea32431aaf2
vitorbite <vitorbitencourt20@gmail.com>
2025-07-30 19:04:44 -0300

    Criando a estrutura da interface

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import React from 'react';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Text style={styles.titulo}>App</Text>
+      <TextInput style={styles.input} placeholder='Ex:79003144'/>
+
+      <View style={styles.areaBtn}>
+      
+        <TouchableOpacity style={styles.btn}>
+          <Text>Buscar</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.btn}>
+          <Text>Limpar</Text>
+        </TouchableOpacity>
+      
+      </View>
     </View>
   );
 }
@@ -22,7 +31,29 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+  },
+  titulo:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  input:{
+    width: '90%',
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#222',
+    borderRadius: 7,
+  },
+  areaBtn: {
+    flexDirection: 'row',
+    width: '90%',
+  },
+  btn:{
+    flex: 1,
+    margin: 50,
+    width: 40,
+    height: 40,
+    backgroundColor: '#000',
   },
 });
-
-export default App;
